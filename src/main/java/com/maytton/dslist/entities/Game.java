1b84ee3b0e66a1b0db68aee1fc44b73347c4eba3
Maytton <maytton0000@gmail.com>
2023-05-08 23:58:45 -0300

    Game seed

--- src/main/java/com/maytton/dslist/entities/Game.java
+++ src/main/java/com/maytton/dslist/entities/Game.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_name")
+@Table(name = "tb_game")
 public class Game {
 	
 	@Id
@@ -21,25 +21,31 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	private String longDescrioption;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescrioption) {	
+	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+			String shortDescription, String longDescrioption,Double score) {	
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescrioption = longDescrioption;
+		this.longDescription = longDescription;
 		
 	}
 
@@ -76,11 +82,20 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -100,11 +115,11 @@ public class Game {
 	}
 
 	public String getLongDescrioption() {
-		return longDescrioption;
+		return longDescription;
 	}
 
-	public void setLongDescrioption(String longDescrioption) {
-		this.longDescrioption = longDescrioption;
+	public void setLongDescrioption(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
